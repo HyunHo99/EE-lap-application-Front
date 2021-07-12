46b1101eb4e48cce6c769e099428bb1bded111fc
NayeonJung <friheyelow@kaist.ac.kr>
2021-07-13 03:50:51 +0900

    navigation 수정

--- app/src/main/java/com/example/myapplication/Fragment/FragPost.kt
+++ app/src/main/java/com/example/myapplication/Fragment/FragPost.kt
@@ -56,8 +56,7 @@ class FragPost : Fragment() {
 
 
         addPostBt.setOnClickListener{ view ->
-//            if(globalVar.equals("0")){
-            if (false) {
+            if(globalVar.equals("0")){
                 Toast.makeText(activity, "게시물 등록은 로그인 후 가능합니다", Toast.LENGTH_SHORT).show()
             }
             else {
